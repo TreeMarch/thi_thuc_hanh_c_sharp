e0b15ca3a691fea21685d698602c2a8dda6accfd
TreeMarch <m5c0traan@gmail.com>
2024-07-24 19:41:13 +0700

    push

--- ExamPaper/Exercise_2/SearchWord.cs
+++ ExamPaper/Exercise_2/SearchWord.cs
@@ -5,23 +5,22 @@ public class SearchWord
 {
     static void Main(string[] args)
     {
-        
-            Console.Write("Enter a string: ");
-            string inputString = Console.ReadLine();
+        int count = 0;
+        int index = -1;
 
-            Console.Write("Enter a word to search: ");
-            string searchWord = Console.ReadLine();
+        Console.Write("Enter a string: ");
+        string inputString = Console.ReadLine();
 
-            int count = 0;
-            int index = inputString.IndexOf(searchWord, 0);
+        Console.Write("Enter a word to search: ");
+        string searchWord = Console.ReadLine();
 
-            while (index!= -1)
-            {
-                count++;
-                index = inputString.IndexOf(searchWord, index + 1);
-            }
+        do
+        {
+            index = inputString.IndexOf(searchWord, index + 1);
+            if (index != -1) count++;
+        } while (index != -1);
 
-            Console.WriteLine($"Word found {count} times in the string.");
+        Console.WriteLine($"Word found {count} times in the string.");
         }
     }
     
